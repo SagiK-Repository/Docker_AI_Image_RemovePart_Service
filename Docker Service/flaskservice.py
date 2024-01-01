060c8b9f7d64b7e1fd956cc9ffaf692d32b30019
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-01-01 22:13:19 +0900

    feat: upload save button 기능 추가 (#7)

--- Docker Service/flaskservice.py
+++ Docker Service/flaskservice.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import tensorflow as tf
 import numpy as np
 import base64
 from PIL import Image
@@ -9,25 +8,40 @@ import io
 app = Flask(__name__)
 CORS(app) # 클라이언트로부터의 요청에 대해 모든 도메인에서 접근 허용(CORS 허용)
 
-# 모델 불러오기
-model_path = 'mnist_model.h100'
-model = tf.keras.models.load_model(model_path)
+def inaryImageToImage(image_data):
+    # base64 디코딩하여 이미지 바이너리 데이터 추출
+    image_binary = base64.b64decode(image_data.split(',')[1])
+
+    # 바이너리 데이터를 Pillow(PIL) Image 객체로 변환
+    image = Image.open(io.BytesIO(image_binary))
+
+    return image
 
 @app.route('/', methods=['GET'])
 def index():
     return "Hello, World!"
 
-@app.route('/predict', methods=['POST'])
+@app.route('/uploadImage', methods=['POST'])
 def predict():
     # 이미지 데이터 받아오기 (JSON 형태)
     image_data_json=request.get_json()
     image_data = image_data_json['image_data']
 
-    # base64 디코딩하여 이미지 바이너리 데이터 추출
-    image_binary = base64.b64decode(image_data.split(',')[1])
+    image = inaryImageToImage(image_data)
 
-    # 바이너리 데이터를 Pillow(PIL) Image 객체로 변환
-    image = Image.open(io.BytesIO(image_binary)).convert('L') # 그레이 스케일로 변환
+    image.save('./image.png')
+
+
+@app.route('/running', methods=['POST'])
+def predict():
+    
+    # 이미지 데이터 받아오기 (JSON 형태)
+    image_data_json=request.get_json()
+    image_data = image_data_json['image_data']
+
+    image = inaryImageToImage(image_data)
+
+    image.save('./image_mask.png')
 
     # 이미지 크기 조정 및 전처리 수행 (MNIST 모델 기준)
     image = Image.eval(image, lambda x: 255 - x) # 이미지 색상 반전
